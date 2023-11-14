f41ead2ed6303b63664e8147378b7ce2453e63c5
Subin <80036437+so0oppy@users.noreply.github.com>
2023-11-14 17:23:31 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int freq[26];
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	string S;
+	cin >> S;
+	
+	for(auto c : S){
+		freq[c - 'a']++;
+	}
+	for(int i=0; i<26; i++){
+		cout << freq[i] << ' ';
+	}
+}
