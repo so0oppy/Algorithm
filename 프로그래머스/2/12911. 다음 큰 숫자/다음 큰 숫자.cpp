0147b736aec4c9c0e07e33b44bbe02c10af25cb0
SuBin Han <80036437+so0oppy@users.noreply.github.com>
2024-12-29 02:50:18 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.84 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,14 +1,25 @@
 #include <string>
-#include <bitset>
+#include <cmath>
 
 using namespace std;
 
-int solution(int n) {
-    int numOnes = bitset<32>(n).count(); // 현재 숫자의 1의 개수
-    while (true) {
-        n++; // 다음 숫자로 증가
-        if (bitset<32>(n).count() == numOnes) { // 1의 개수가 같으면 반환
-            return n;
-        }
+int countOnes(int m){
+    int cnt = 0; // 1의 갯수
+    
+    while(m>0)
+    {
+        if(m&1) cnt++; // n의 비트 끝자리가 1인 경우
+        m = m >> 1; // n의 비트를 오른쪽으로 한 칸 이동
     }
+    return cnt;
+}
+
+int solution(int n) {
+    int i = 1;
+    while(1)
+    {
+        if(countOnes(n) == countOnes(n+i)) return n+i;
+        i++;
+    }
+    
 }
