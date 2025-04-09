e2f1421abdaa99c0550f1ffa2f5edb9ff99513a3
SuBin Han <80036437+so0oppy@users.noreply.github.com>
2025-04-09 14:30:45 +0900

    [Silver IV] Title: 대피소, Time: 20 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/28215. 대피소/대피소.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, k;
+int x[50], y[50];
+int calc_dist(int i, int j, int k){
+    int res = 0;
+    for(int h=0; h<n; h++){
+        res = max(res, min(min(abs(x[i]-x[h])+abs(y[i]-y[h]), abs(x[j]-x[h])+abs(y[j]-y[h])), 
+                  abs(x[k]-x[h])+abs(y[k]-y[h])));
+    }
+    return res;
+}
+
+int main(){
+    int answer = 1e9;
+    cin >> n >> k;
+    for(int i=0; i<n; i++){
+        cin >> x[i] >> y[i];
+    }
+    if(k==1){
+        for(int i=0; i<n; i++){
+            answer = min(answer, calc_dist(i, i, i));
+        }
+    }
+    else if(k==2){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                answer = min(answer, calc_dist(i, j, j));
+            }
+        }
+    }
+    else{
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                for(int k=0; k<n; k++){
+                    answer = min(answer, calc_dist(i, j, k));
+                }
+            }
+        }
+    }
+    cout << answer;
+    return 0;
+}
