b7385dfb2a97ac7877e7d8b75411ddfb6709cab2
SuBin Han <80036437+so0oppy@users.noreply.github.com>
2025-04-29 10:22:05 +0900

    [level 2] Title: 주식가격, Time: 26.58 ms, Memory: 26.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices)
+{
+    vector<int> answer(prices.size());
+    stack<int> s;
+
+    prices[prices.size() - 1] = 0;
+
+    for (int i = 0; i < prices.size(); ++i)
+    {
+        while (!s.empty() && prices[s.top()] > prices[i])
+        {
+            answer[s.top()] = i - s.top();
+            s.pop();
+        }
+        s.push(i);
+    }
+    
+    while(!s.empty())
+    {
+        answer[s.top()] = prices.size() - s.top() - 1;
+        s.pop();
+    }
+
+    return answer;
+}
